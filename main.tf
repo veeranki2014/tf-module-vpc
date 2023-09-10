84a83f5a3a59fbee09dd471fe84b758f59abfc19
veeranki2014 <veeranki2014@gmail.com>
2023-09-10 00:35:49 -0500

    aws-terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
+
+  tags = merge ({
+    Name = "${var.env}-vpc"
+  },
+    var.tags )
+}
+
+
+resource "aws_subnet" "main" {
+  count      = length(var.web_subnet_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.web_subnet_cidr_block, count.index )
+
+  tags = merge ({
+    Name = "${var.env}-web-subnet"
+  },
+    var.tags )
 }
