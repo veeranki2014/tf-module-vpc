5ac0b6df17ea29b6fde4f93f38a1207f959a3396
veeranki2014 <veeranki2014@gmail.com>
2023-09-10 01:09:30 -0500

    aws-terraform

--- main.tf
+++ main.tf
@@ -8,13 +8,14 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_subnet" "main" {
-  count      = length(var.web_subnet_cidr_block)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.web_subnet_cidr_block, count.index )
+module "subnets" {
+  source = "./subnets"
 
-  tags = merge ({
-    Name = "${var.env}-web-subnet"
-  },
-    var.tags )
+  for_each = var.subnets
+  cidr_block = each.value["cidr_block"]
+  subnet_name = each.key
+  vpc_id      = aws_vpc.main.id
+  env    = var.env
+  tags   = var.tags
+  az     = var.az
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
 variable "cidr_block" {}
 variable "tags" {}
 variable "env" {}
-variable "web_subnet_cidr_block" {}
+variable "subnets" {}
+variable "az" {
+  default = ["us-east-1a", "us-east-1b"]
+}
