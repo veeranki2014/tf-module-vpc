0064ddb476c4fe02f9fd2f8fc47a909d94709291
veeranki2014 <veeranki2014@gmail.com>
2023-09-10 16:52:00 -0500

    aws-terraform

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "subnets" {
   source = "./subnets"
 
   for_each = var.subnets
-  cidr_block = each.value["subnets"]
+  cidr_block = each.value["cidr_block"]
   subnet_name = each.key
   vpc_id      = aws_vpc.main.id
   env    = var.env
