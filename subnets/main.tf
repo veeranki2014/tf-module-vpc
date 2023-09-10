9d9a00dcf867ca92903f2f6f46f58efc68976373
veeranki2014 <veeranki2014@gmail.com>
2023-09-10 16:08:10 -0500

    aws-terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
   count      = length(var.subnets)
   vpc_id     = var.vpc_id
-  cidr_block = element(var.cidr_block, count.index )
+  cidr_block = element(var, count.index )
   availability_zone = element(var.az, count.index )
 
   tags = merge ({
