c754b24b463018f2044d6ba0cb56e0a662c2b195
veeranki2014 <veeranki2014@gmail.com>
2023-09-23 13:24:26 -0500

    terraform vpc peering

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_vpc_peering_connection" "peer" {
   #peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.default_vpc_id
+  auto_accept   = true
 }
 
 
